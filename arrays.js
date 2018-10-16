4cab17b1b1bd7074c7d5644fda2ccbb743318687
Luis Zabala <luis.zabala076@gmail.com>
2018-10-16 19:42:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (array, element) {;
-  var array2 = "foo";
+  var array = "foo";
   element = 1;
-  return ()
+  return (element, array)
 }
