6e6bf467157593f644ebb1edfa9b5b69c935ab49
Luis Zabala <luis.zabala076@gmail.com>
2018-10-16 19:26:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 var phephiphofump = ["break", "Fix", "Create"]
 
-function addElementToBeginningOfArray ([1], foo) {
-var array = [1,2,3]
+function addElementToBeginningOfArray () {
   
 }
 
-function destructivlyAddElementToBeginningOfArray ([phephiphofump], ){
-
+function destructivlyAddElementToBeginningOfArray ()
   
 }
