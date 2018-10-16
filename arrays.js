7852e32933fe266caf82e17faef6ec6ab60dd182
Luis Zabala <luis.zabala076@gmail.com>
2018-10-16 19:55:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,8 @@ var newElement = "foo"
 
 function addElementToBeginningOfArray (chocolateBars,newElement) {
   
+}
 
+function addElementToBeginningOfArray (chocolateBars, newElement){
+  chocolateBars.unshift(1)
 }
