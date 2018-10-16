4364ef1fc23744b3e9e869d0b176328cd8076ae3
Luis Zabala <luis.zabala076@gmail.com>
2018-10-16 19:07:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,11 @@
 
+
 function addElementToBeginningOfArray (MM) {
   var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 }
 
 function destructivelyAddElementToBeggingOfArray(PP) {
-
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
   
 }
 
