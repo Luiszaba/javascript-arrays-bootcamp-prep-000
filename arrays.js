5b7e200b58281c6ef093bcd2dc662759feee8710
Luis Zabala <luis.zabala076@gmail.com>
2018-10-16 19:38:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-var foo = [3,5,7]
+
 function addElementToBeginningOfArray (array, element) {
 }
 
 
 function addElementToBeginningOfArray(array, [1]) {
-  array.unshift([1])
+  var
 }
