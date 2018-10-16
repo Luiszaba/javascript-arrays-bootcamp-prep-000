623245b821d3df01d870d107955caa349fc05dbd
Luis Zabala <luis.zabala076@gmail.com>
2018-10-16 20:18:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ function addElementToBeginningOfArray (array, element) {
   return [element,...array]
 }
 
+function desturctivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
